2c33cd8d548718d5decd2a67842032c9a3877b3a
anastasia berishvili <berishvilianastasia250@gmail.com>
2023-02-20 16:03:51 +0400

    my second commit

--- HOMEWORK15.py
+++ HOMEWORK15.py
@@ -2,3 +2,8 @@ x1 = int(input("x1: "))
 x2 = int(input("x2: "))   
 y1 = int(input("y1: "))   
 y2 = int(input("y2: "))
+if (1 <= x1 <= 8) and (1 <= x2 <= 8) and (1 <= y1 <= 8) and (1 <= y2 <= 8):
+    print("ok!")
+
+else:
+     print("enter number from range(1, 8)!")
